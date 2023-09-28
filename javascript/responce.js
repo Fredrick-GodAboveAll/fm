99d4a37666f8a67248ddac8f2bfa4f27c371ce16
Fredrick-GodAboveAll <fredrickmuasya553@gmail.com>
2023-09-28 19:05:54 +0300

    prior to git hub push

--- javascript/responce.js
+++ javascript/responce.js
@@ -1,5 +1,24 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .nav');
+let header = document.querySelector('.header');
 
-// other js interactivity goes here
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+
+    if(window.scrollY > 0) {
+        header.classList.add('active');
+    } else {
+        header.classList.remove('active');
+    }
+    
+}
 
 
 /*=============== SWIPER PROJECTS ===============*/
@@ -26,3 +45,6 @@ let swiperProjects = new Swiper(".projects__container", {
     },
 
 });
+
+
+// swiper ends
