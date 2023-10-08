d9b375681a285055156a83203046ebe44ae4a5bb
Fredrick-GodAboveAll <fredrickmuasya553@gmail.com>
2023-10-08 16:35:03 +0300

    news

--- javascript/responce.js
+++ javascript/responce.js
@@ -23,33 +23,6 @@ window.onscroll = () => {
 
 /*=============== SWIPER PROJECTS ===============*/
 
-let swiperProjects = new Swiper(".projects__container", {
-
-    // cssMode: true,
-    loop: true,
-    spaceBetween: 24,
-
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-        el: ".swiper-pagination",
-    },
-
-    breakpoints: {
-        1200: {
-            slidesPerView: 2,
-            spaceBetween: -56,
-        },
-    },
-
-});
-
-
-// swiper ends
-
-
 
 /*=============== EMAIL JS ===============*/
 
